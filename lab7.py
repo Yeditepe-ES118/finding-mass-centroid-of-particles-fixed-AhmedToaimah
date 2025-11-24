4fd4bfb029d47358a21989d78764a4cdc6e7200d
AhmedToaimah <ahmed.toaimah@std.yeditepe.edu.tr>
2025-11-24 16:55:37 +0300

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def centroid(p1x, p1y, p2x, p2y, p3x, p3y, m1, m2, m3):
+    positions = np.array([[p1x, p2x, p3x],
+                          [p1y, p2y, p3y]])
+
+    masses = np.array([m1, m2, m3])
+
+    tot_mass = np.sum(masses)
+
+    cx = np.sum(positions[0, :] * masses) / tot_mass
+    cy = np.sum(positions[1, :] * masses) / tot_mass
+
+    plt.plot(positions[1, :], positions[1, :], "bo")
+    plt.plot(cx, cy, "r+")
+    plt.savefig("test.png")
+    plt.close()
+
+centroid(1,1,0,1,1,0,3,4,5)
